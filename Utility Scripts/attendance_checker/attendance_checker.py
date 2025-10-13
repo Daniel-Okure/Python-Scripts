2a1f70eb6fdebc7694b783e8a2f56e013c8ff3ef
Daniel-Okure <danielihokure@gmail.com>
2025-10-13 08:50:58 +0100

    Made some improvements

--- Utility Scripts/attendance_checker/attendance_checker.py
+++ Utility Scripts/attendance_checker/attendance_checker.py
@@ -1,25 +1,32 @@
 import asyncio
 from playwright.async_api import async_playwright
 
-async def attendance_checker(url, username, password, mode="Lecture Attendance"):
+print("Enter your portal username: ")
+user = input().strip() # Username
+
+print("Enter your portal password: ")
+passwd = input().strip() # Password
+
+print("What do you want to check: Lecture Attendance or Student Affairs?")
+choice = input().strip()
+
+async def attendance_checker(url, username, password, mode):
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=True) # Set to False if you want to see the magic :)
+        browser = await p.chromium.launch(headless=False) # Set to False if you want to see the magic :)
         context = await browser.new_context()
         page = await context.new_page()
         await page.goto(url, wait_until="networkidle", timeout=0)
         
-        user = username # Username
-        passwd = password # Password
-        mode_options = ["Lecture Attendance", "Student Affairs"]
+        mode_options = ["lecture attendance", "student affairs"]
         
-        await page.locator("input[name='userid']").fill(user)
-        await page.locator("input[name='inputpassword1']").fill(passwd)
+        await page.locator("input[name='userid']").fill(username)
+        await page.locator("input[name='inputpassword1']").fill(password)
         await page.click("input[type='submit']")
         
         await page.click("text=attendance", timeout=0)
         
         # Navigate to the attendance you want to check
-        if mode == "Lecture Attendance":
+        if mode.lower() == "lecture attendance":
             await page.click("text=Lecture Attendance Reports", timeout=0)
             await page.click("text=Attendance Summay For Student", timeout=0) # lol typo
             
@@ -28,7 +35,7 @@ async def attendance_checker(url, username, password, mode="Lecture Attendance")
             await frame.locator("div.col-md-10").wait_for(state="visible")
             await frame.locator("div.col-md-10").screenshot(path="lt_attendance.png")
             
-        elif mode == "Student Affairs":
+        elif mode.lower() == "student affairs":
             await page.click("text=Student Affairs Attendance", timeout=0)
             await page.click("text=Attendance Summary For Student", timeout=0)
             
@@ -42,10 +49,10 @@ async def attendance_checker(url, username, password, mode="Lecture Attendance")
             await asyncio.sleep(10) # Waits for the Roll Call to load
             await frame.locator("div#upload-report").screenshot(path="roll_call.png")
             
-        elif mode not in mode_options:
+        elif mode.lower() not in mode_options:
             print("Invalid mode: Lecture Attendance or Student Affairs")
         
         await browser.close()
         
 portal_url = "https://cuportal.covenantuniversity.edu.ng/login.php"
-asyncio.run(attendance_checker(portal_url, "", "")) # Input your username and password here
+asyncio.run(attendance_checker(portal_url, user, passwd, choice))
