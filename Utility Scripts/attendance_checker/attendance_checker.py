1d61a48c6a802d17777cdd1654a0b1e802a8fb7e
Daniel-Okure <danielihokure@gmail.com>
2025-10-15 21:20:46 +0100

    Made some improvements

--- Utility Scripts/attendance_checker/attendance_checker.py
+++ Utility Scripts/attendance_checker/attendance_checker.py
@@ -7,7 +7,7 @@ user = input().strip() # Username
 print("Enter your portal password: ")
 passwd = input().strip() # Password
 
-print("What do you want to check: Lecture Attendance or Student Affairs?")
+print("What do you want to check: Lecture Attendance (1) or Student Affairs (2)?")
 choice = input().strip()
 
 async def attendance_checker(url, username, password, mode):
@@ -17,41 +17,43 @@ async def attendance_checker(url, username, password, mode):
         page = await context.new_page()
         await page.goto(url, wait_until="networkidle", timeout=0)
         
-        mode_options = ["lecture attendance", "student affairs"]
+        mode_options = ["1", "2"]
         
-        await page.locator("input[name='userid']").fill(username)
-        await page.locator("input[name='inputpassword1']").fill(password)
-        await page.click("input[type='submit']")
+        if mode in mode_options:
+            
+            await page.locator("input[name='userid']").fill(username)
+            await page.locator("input[name='inputpassword1']").fill(password)
+            await page.click("input[type='submit']")
         
-        await page.click("text=attendance", timeout=0)
+            await page.click("text=attendance", timeout=0)
         
-        # Navigate to the attendance you want to check
-        if mode.lower() == "lecture attendance":
-            await page.click("text=Lecture Attendance Reports", timeout=0)
-            await page.click("text=Attendance Summay For Student", timeout=0) # lol typo
+            # Navigate to the attendance you want to check
+            if mode == "1":
+                await page.click("text=Lecture Attendance Reports", timeout=0)
+                await page.click("text=Attendance Summay For Student", timeout=0) # lol typo
             
-            # Get Lecture Attendance
-            frame = page.frame_locator("iframe#frame")
-            await frame.locator("div.col-md-10").wait_for(state="visible")
-            await frame.locator("div.col-md-10").screenshot(path="lt_attendance.png")
+                # Get Lecture Attendance
+                frame = page.frame_locator("iframe#frame")
+                await frame.locator("div.col-md-10").wait_for(state="visible")
+                await frame.locator("div.col-md-10").screenshot(path="lt_attendance.png")
             
-        elif mode.lower() == "student affairs":
-            await page.click("text=Student Affairs Attendance", timeout=0)
-            await page.click("text=Attendance Summary For Student", timeout=0)
+            elif mode == "2":
+                await page.click("text=Student Affairs Attendance", timeout=0)
+                await page.click("text=Attendance Summary For Student", timeout=0)
             
-            # Get Chapel Attendance
-            frame = page.frame_locator("iframe#frame")
-            await frame.locator("div#upload-report").wait_for(state="visible")
-            await frame.locator("div#upload-report").screenshot(path="chapel_attendance.png")
+                # Get Chapel Attendance
+                frame = page.frame_locator("iframe#frame")
+                await frame.locator("div#upload-report").wait_for(state="visible")
+                await frame.locator("div#upload-report").screenshot(path="chapel_attendance.png")
             
-            # Get Roll Call
-            await frame.locator("li#event3").click()
-            await asyncio.sleep(10) # Waits for the Roll Call to load
-            await frame.locator("div#upload-report").screenshot(path="roll_call.png")
+                # Get Roll Call
+                await frame.locator("li#event3").click()
+                await asyncio.sleep(10) # Waits for the Roll Call to load
+                await frame.locator("div#upload-report").screenshot(path="roll_call.png")
+                
+        else:
+            print("Invalid mode: Lecture Attendance (1) or Student Affairs (2)")
             
-        elif mode.lower() not in mode_options:
-            print("Invalid mode: Lecture Attendance or Student Affairs")
-        
         await browser.close()
         
 portal_url = "https://cuportal.covenantuniversity.edu.ng/login.php"
