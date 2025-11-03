88b3695596aa3ffc89969114d0bcd3ca124b13e4
Daniel-Okure <danielihokure@gmail.com>
2025-11-03 06:33:49 +0100

    Made some improvements

--- Utility Scripts/ffmpeg_converter.py
+++ Utility Scripts/ffmpeg_converter.py
@@ -2,25 +2,22 @@ import subprocess
 from pathlib import Path
 
 
-def ffmpeg_converter():
-    video_path = Path.home() / "Videos"
+def video_converter(dest, video):
     music_path = Path.home() / "Music"
-    videos = video_path.iterdir()
-    
-    
-    for video in videos:
         
-        if video.name.endswith((".ts", ".mp4", ".mkv", ".webm")):
-            name = video.stem
-            output_path = music_path / f"{name}.mp3"
+    video_name = video.stem
+    output_path = music_path / f"{video_name}{dest}"
     
-            command = ["ffmpeg", "-i", video, "-q:a", "0", "-map", "a", output_path]  # Requires ffmpeg
-            subprocess.run(command, check=True)
-            print(f"Converted: {video} -> {output_path}")
+    command = ["ffmpeg", "-i", str(video), "-q:a", "0", "-map", "a", str(output_path)]  # Requires ffmpeg
+    subprocess.run(command, check=True)
+    print(f"Converted: {video} -> {output_path}")
 
 
 if __name__ == "__main__":
-    ffmpeg_converter()
+    video_path = Path(input("Enter absolute path of specific video: ").strip())
+    dest_format = input('Enter desired format (e.g., ".wav"): ').strip()
+
+    video_converter(dest_format, video_path)
 
     
 
