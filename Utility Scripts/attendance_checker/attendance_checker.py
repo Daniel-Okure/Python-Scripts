fe6282d16353edbccecd09879cae694915c533da
Daniel-Okure <danielihokure@gmail.com>
2025-11-22 08:51:34 +0100

    Made some improvements

--- Utility Scripts/attendance_checker/attendance_checker.py
+++ Utility Scripts/attendance_checker/attendance_checker.py
@@ -15,8 +15,10 @@ print("Logging in...")
 
 async def attendance_checker(url, username, password, mode):
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=True) # Set to False if you want to see the magic :)
-        context = await browser.new_context()
+        browser = await p.chromium.launch(headless=True, args=["--disable-blink-features=AutomationControlled, --disable-infobars"]) # Set to False if you want to see the magic :)
+        context = await browser.new_context(
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Safari/537.36",
+        )
         page = await context.new_page()
         await page.goto(url, wait_until="networkidle", timeout=0)
         
@@ -52,25 +54,38 @@ async def attendance_checker(url, username, password, mode):
             
                 # Get Lecture Attendance
                 frame = page.frame_locator("iframe#frame")
-                await frame.locator("div.col-md-10").wait_for(state="visible", timeout=0)
-                await frame.locator("div.col-md-10").screenshot(path="lt_attendance.png")
-                print(f"lt_attendance.png saved to {Path.cwd()}")
+                
+                if await frame.locator("div#sub-frame-error").is_visible():
+                    print("The page is currently unavailable. Try again at another time. \n\nExiting...")
+                    await browser.close()
+                else:
+                    await frame.locator("div.col-md-10").wait_for(state="visible", timeout=0)
+                    await frame.locator("div.col-md-10").screenshot(path="lt_attendance.png")
+                    print(f"lt_attendance.png saved to {Path.cwd()}")
+                
             
             elif mode == "2":
                 await page.click("text=Student Affairs Attendance", timeout=0)
                 await page.click("text=Attendance Summary For Student", timeout=0)
             
-                # Get Chapel Attendance
                 frame = page.frame_locator("iframe#frame")
-                await frame.locator("div#upload-report").wait_for(state="visible", timeout=0)
-                await frame.locator("div#upload-report").screenshot(path="chapel_attendance.png")
-                print(f"chapel_attendance.png saved to {Path.cwd()}")
-            
-                # Get Roll Call
-                await frame.locator("li#event3").click()
-                await asyncio.sleep(10) # Waits for the Roll Call to load
-                await frame.locator("div#upload-report").screenshot(path="roll_call.png")
-                print(f"roll_call.png saved to {Path.cwd()}")
+                
+                if await frame.locator("div#sub-frame-error").is_visible():
+                    print("The page is currently unavailable. Try again at another time. \n\nExiting...")
+                    await browser.close()
+                
+                else:
+                    # Get Chapel Attendance
+                    await frame.locator("div#upload-report").wait_for(state="visible", timeout=0)
+                    await frame.locator("div#upload-report").screenshot(path="chapel_attendance.png")
+                    print(f"chapel_attendance.png saved to {Path.cwd()}")
+                    
+                    # Get Roll Call
+                    await frame.locator("li#event3").click()
+                    await asyncio.sleep(10)  # Waits for the Roll Call to load
+                    await frame.locator("div#upload-report").screenshot(path="roll_call.png")
+                    print(f"roll_call.png saved to {Path.cwd()}")
+                
                 
         else:
             print("Invalid mode: Lecture Attendance (1) or Student Affairs (2)")
